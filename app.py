a48fadfe06a38ff26854862fe7e4f2c766090c3c
Fuji <jifu7833@gmail.com>
2024-04-25 14:43:22 +0900

    チャンネル一覧の作成

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 
+#チャンネル一覧
+@app.route('/')
+def index():
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect('/login')
 
+    return render_template('index.html')
+
+
+
+if __name__ =='__main__':
+    app.run()
