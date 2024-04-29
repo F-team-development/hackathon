84aac9e6e07fb0debb1c4d4b36600f89705295ca
Fuji <jifu7833@gmail.com>
2024-04-29 17:18:03 +0900

    init.sqlを変更

--- Fチーム/Docker/MySQL/init.sql
+++ Fチーム/Docker/MySQL/init.sql
@@ -1,11 +1,11 @@
 
-DROP DATABASE chatapp;
+DROP DATABASE ChatPET;
 DROP USER 'testuser';
 
 CREATE USER 'testuser' IDENTIFIED BY 'testuser';
-CREATE DATABASE chatapp;
+CREATE DATABASE ChatPET;
 USE chatapp
-GRANT ALL PRIVILEGES ON chatapp.* TO 'testuser';
+GRANT ALL PRIVILEGES ON ChatPET.* TO 'testuser';
 
 CREATE TABLE users (
     uid varchar(255) PRIMARY KEY,
@@ -17,18 +17,29 @@ CREATE TABLE users (
 CREATE TABLE channels (
     id serial PRIMARY KEY,
     uid varchar(255) REFERENCES users(uid),
-    name varchar(255) UNIQUE NOT NULL,
-    abstract varchar(255)
+    name varchar(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE messages (
     id serial PRIMARY KEY,
     uid varchar(255) REFERENCES users(uid),
     cid integer REFERENCES channels(id) ON DELETE CASCADE,
-    message text,
-    created_at timestamp not null default current_timestamp
+    message text
 );
 
-INSERT INTO users(uid, user_name, email, password)VALUES('970af84c-dd40-47ff-af23-282b72b7cca8','テスト','test@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578');
-INSERT INTO channels(id, uid, name, abstract)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','ぼっち部屋','テストさんの孤独な部屋です');
-INSERT INTO messages(id, uid, cid, message)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8', '1', '誰かかまってください、、')
+CREATE TABLES posts(
+    post_id serial PRIMARY KEY,
+    post text NOT NULL,
+    uid VARCHAR(255) REFERENCES users(uid)
+);
+
+CREATE TABLES images(
+    image_id SERIAL PRIMARY KEY,
+    post_id INT NOT NULL,
+    image_data BLOB NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts(post_id) 
+);
+
+INSERT INTO users(uid, user_name, email, password)VALUES('99ace1a1-725b-be03-2455-56a66be4a079','テスト','pet@gmail.com','778c04806dd4ba98b1bbff9083433a8f67aed60a3346dfa16913fb5e875e462d');
+INSERT INTO channels(id, uid, name)VALUES(1, '99ace1a1-725b-be03-2455-56a66be4a079','犬');
+INSERT INTO messages(id, uid, cid, message)VALUES(1, '99ace1a1-725b-be03-2455-56a66be4a079', '1', '犬です')
