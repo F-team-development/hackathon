5e90c8530277925cce9ce7d40e095f09fcacecaf
Fuji <jifu7833@gmail.com>
2024-04-30 15:31:13 +0900

    models.pyの修正

--- Fチーム/ChatApp/models.py
+++ Fチーム/ChatApp/models.py
@@ -39,8 +39,7 @@ class dbConnect:
             channels = cur.fetchall()
             return channels
         except Exception as e:
-            print(e + 'が発生しています')
-            abort(500)
+            print(f'エラーが発生しています：{e}')
         finally:
             cur.close()
 
@@ -54,8 +53,7 @@ class dbConnect:
             channel = cur.fetchone()
             return channel
         except Exception as e:
-            print(e + 'が発生しています')
-            abort(500)
+            print(f'エラーが発生しています:{e}')
         finally:
             cur.close()
 
@@ -69,22 +67,7 @@ class dbConnect:
             channel = cur.fetchone()
             return channel
         except Exception as e:
-            print(e + 'が発生しています')
-            abort(500)
-        finally:
-            cur.close()
-
-
-    def addChannel(uid, newChannelName, newChannelDescription):
-        try:
-            conn = DB.getConnection()
-            cur = conn.cursor()
-            sql = "INSERT INTO channels (uid, name, abstract) VALUES (%s, %s, %s);"
-            cur.execute(sql, (uid, newChannelName, newChannelDescription))
-            conn.commit()
-        except Exception as e:
-            print(e + 'が発生しています')
-            abort(500)
+            print(f'エラーが発生しています：{e}')
         finally:
             cur.close()
 
@@ -97,8 +80,7 @@ class dbConnect:
             cur.execute(sql, (channel_name))
             channel = cur.fetchone()
         except Exception as e:
-            print(e + 'が発生しました')
-            abort(500)
+            print(f'エラーが発生しています:{e}')
         finally:
             cur.close()
             return channel
@@ -112,23 +94,7 @@ class dbConnect:
             cur.execute(sql, (uid, newChannelName, newChannelDescription, cid))
             conn.commit()
         except Exception as e:
-            print(e + 'が発生しました')
-            abort(500)
-        finally:
-            cur.close()
-
-
-    #deleteチャンネル関数
-    def deleteChannel(cid):
-        try:
-            conn = DB.getConnection()
-            cur = conn.cursor()
-            sql = "DELETE FROM channels WHERE id=%s;"
-            cur.execute(sql, (cid))
-            conn.commit()
-        except Exception as e:
-            print(e + 'が発生しています')
-            abort(500)
+            print(f'エラーが発生しています：{e}')
         finally:
             cur.close()
 
@@ -142,8 +108,7 @@ class dbConnect:
             messages = cur.fetchall()
             return messages
         except Exception as e:
-            print(e + 'が発生しています')
-            abort(500)
+            print(f'エラーが発生しています：{e}')
         finally:
             cur.close()
 
@@ -156,8 +121,7 @@ class dbConnect:
             cur.execute(sql, (uid, cid, message))
             conn.commit()
         except Exception as e:
-            print(e + 'が発生しています')
-            abort(500)
+            print(f'エラーが発生しています：{e}')
         finally:
             cur.close()
 
@@ -170,7 +134,6 @@ class dbConnect:
             cur.execute(sql, (message_id))
             conn.commit()
         except Exception as e:
-            print(e + 'が発生しています')
-            abort(500)
+            print(f'エラーが発生しています：{e}')
         finally:
             cur.close()
